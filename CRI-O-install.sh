70c09c0d34abf8eb40d96bbf03a9cdf02eb8ca6f
micki <github@com-science.de>
2020-09-27 20:46:47 +0200

    Add automatic install

--- CRI-O-install.sh
+++ CRI-O-install.sh
@@ -21,7 +21,7 @@ curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:sta
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
 
 apt-get update
-apt-get install cri-o cri-o-runc
+apt-get install -y cri-o cri-o-runc
 
 systemctl daemon-reload
 systemctl start crio
